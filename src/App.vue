3daefc61d9e83018f32d183932db29de526137fd
randeer <randeerlalanga92@gmail.com>
2020-04-24 10:41:35 +0000

    footeradded

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,21 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <Footer />
   </div>
 </template>
+
+<script>
+import Footer from '@/components/Footer'
+export default {
+  name: 'app',
+  components: {
+    Footer
+  }
+  
+}
+</script>
+
 
 <style>
 #app {
